1a658b2f63d21f58d4d1b36330741ae8cf54956e
Jeffrey Schoemaker <jeffrey.schoemaker@perplex.nl>
2019-05-20 14:52:21 +0200

    Make it Grid compatible

--- umbraco-trix.js
+++ umbraco-trix.js
@@ -1,12 +1,13 @@
 app.requires.push('angularTrix');
 angular.module("umbraco").controller("Umbraco.Core.TrixController",
-function ($scope, assetsService, dialogService) {
-
+function ($scope, assetsService) {
+   
 	assetsService
 		.load([
 			"~/App_Plugins/Trix/trix.js"
-		]).then(function() {
-		});
+        ]).then(function () {
+            Trix.config.blockAttributes.default.tagName = 'p';
+    });
 
 	// load the separate css for the editor to avoid it blocking our JavaScript loading
 	assetsService.loadCss("~/App_Plugins/Trix/trix.css");
